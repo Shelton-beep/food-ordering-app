75930a2e853e27a1da9324fddd98d54fe671c942
Shelton Simbi <shltnsimbi@gmail.com>
2023-11-27 10:22:11 +0200

    Created header layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <header>
-        <a href="">Shush Pizza</a>
-        <na>
+      <header className="flex items-center justify-between">
+        <Link className="text-primary font-semibold text-2xl" href="">
+          SHUSHU PIZZA
+        </Link>
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
           <Link href={""}>Home</Link>
           <Link href={""}>Menu</Link>
           <Link href={""}>About</Link>
           <Link href={""}>Contact</Link>
-          <Link href={""}>Login</Link>
-        </na>
+          <Link
+            href={""}
+            className="bg-primary text-white rounded-full px-8 py-2"
+          >
+            Login
+          </Link>
+        </nav>
       </header>
     </>
   );
