83a3f35692d249a092ea13be037d3e1a1071527a
Shelton Simbi <shltnsimbi@gmail.com>
2023-11-27 11:28:12 +0200

    created hero section component

---
+++ src/components/icons/Right.js
@@ -0,0 +1,22 @@
+const Right = () => {
+  return (
+    <>
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+    </>
+  );
+};
+
+export default Right;
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Right from "../icons/Right";
+
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="py-12">
+          <h1 className="text-4xl font-semibold">
+            Everything
+            <br />
+            is better
+            <br />
+            with a&nbsp;
+            <span className="text-primary">Pizza</span>
+          </h1>
+          <p className="my-6 text-gray-500 text-sm">
+            Pizza is the missing piece that makes a day complete, a simple yet
+            delicious joy in life
+          </p>
+          <div className="flex gap-4 text-sm">
+            <button className="bg-primary flex gap-2 text-white uppercase items-center py-2 px-4 rounded-full ">
+              Order now
+              <Right />
+            </button>
+            <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+              Learn more
+              <Right />
+            </button>
+          </div>
+        </div>
+        <div className="relative">
+          <Image
+            src={"/pizzaImage.jpg"}
+            alt={"pizza"}
+            objectFit={"contain"}
+            layout={"fill"}
+          />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
