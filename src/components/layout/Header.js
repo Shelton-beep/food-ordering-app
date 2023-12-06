1a9986727598c450df325a9fb0c587e1521ca184
Shelton Simbi <shltnsimbi@gmail.com>
2023-12-06 10:40:41 +0200

    refactored code

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,19 +4,22 @@ const Header = () => {
   return (
     <>
       <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href="">
-          SHUSHU PIZZA
-        </Link>
         <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link className="text-primary font-semibold text-2xl" href="">
+            SHUSHU PIZZA
+          </Link>
           <Link href={""}>Home</Link>
           <Link href={""}>Menu</Link>
           <Link href={""}>About</Link>
           <Link href={""}>Contact</Link>
+        </nav>
+        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+          <Link href={""}>Login</Link>
           <Link
             href={""}
             className="bg-primary text-white rounded-full px-8 py-2"
           >
-            Login
+            Register
           </Link>
         </nav>
       </header>
