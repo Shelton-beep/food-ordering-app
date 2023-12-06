03ecd9287c72459a6a6ac8ea6e6800c2dcf57661
Shelton Simbi <shltnsimbi@gmail.com>
2023-12-06 11:37:23 +0200

    Added a register page form

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,8 +1,27 @@
+import Image from "next/image";
+
 const RegisterPage = () => {
   return (
     <>
       <section className="mt-8">
-        <h1 className="text-center text-primary text-4xl">Register</h1>
+        <h1 className="text-center text-primary mb-4 text-4xl">Register</h1>
+        <form className="block max-w-xs mx-auto">
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Register</button>
+          <div className="my-4 text-center text-gray-500">
+            or login with provider
+          </div>
+          <button className="flex gap-4 justify-center">
+            <Image
+              src={"/googleImage.png"}
+              alt="LogIn with google"
+              width={24}
+              height={24}
+            />
+            Login with google
+          </button>
+        </form>
       </section>
     </>
   );
